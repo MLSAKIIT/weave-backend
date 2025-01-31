112446f0620437ed1dba948c7aa947bf80d93c18
ashmit1795 <ashmitpatra77@gmail.com>
2025-01-31 21:03:00 +0530

    refactor: remove username field from users table schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,7 +3,6 @@ import { pgTable, varchar, serial, timestamp, text } from "drizzle-orm/pg-core";
 export const usersTable = pgTable("users", {
   id: serial("id").primaryKey(),
   fullName: text("full_name").notNull(),
-  username: varchar("username", {length: 50}).notNull().unique(),
   email: text("email").notNull().unique(),
   passwordHash: text("password_hash").notNull(),
   avatarUrl: text("avatar_url"),
