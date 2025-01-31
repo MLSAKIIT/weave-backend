db97f6fbb79d69012535ab598d3c84e8e1d7fb06
ashmit1795 <ashmitpatra77@gmail.com>
2025-01-31 19:21:37 +0530

    create users table schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,11 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { pgTable, varchar, serial, timestamp, text } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
+  id: serial("id").primaryKey(),
+  fullName: text("full_name").notNull(),
+  username: varchar("username", {length: 50}).notNull().unique(),
+  email: text("email").notNull().unique(),
+  passwordHash: text("password_hash").notNull(),
+  avatarUrl: text("avatar_url"),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
 });
