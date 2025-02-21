baa1a7eaa90879f0489b41acdf135ae07a2a9020
Swayam-Dhungana <swayamdhungana.official@gmail.com>
2025-02-21 20:21:19 +0545

    Websocket Setup

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { userRouter } from "./routes/userRouter";
 import authRouter from "./routes/authRouter";
-
+import { upgradeWebSocket } from "./server";
 const app = new Hono();
 
 
@@ -16,7 +16,18 @@ app.use(
   })
 );
 
-
+app.get('/ws',upgradeWebSocket((c)=>{
+  console.log('User Connected')
+  return{
+    onMessage(event, ws){
+      console.log(`Message from client: ${event.data}`)
+      ws.send('Hello from server')
+    },
+    onClose:()=>{
+      console.log('Connection closed!')
+    }
+  }
+}))
 
 app.route("/api/v1/user", userRouter);
 app.route("/api/v1/auth", authRouter);
